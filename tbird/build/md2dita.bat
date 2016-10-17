da181776ed05224042ec06a1598a652a256e8810
Sean Healy <sean.healy@ingeniux.com>
2016-10-17 16:20:46 -0700

    Remove duplicated build dir from tbird dir

--- tbird/build/md2dita.bat
+++
@@ -1,8 +0,0 @@
-for %%x in (*.md) do multimarkdown -f -b %%x
-cd D:\working\dita-env\dita-ot-2.3.2\plugins\org.dita.specialization.h2d
-REM "Call" allows the script to continue to the cd command
-call ant -Dargs.input=D:\working\mark -Dargs.output=D:\working\mark
-cd D:\working\mark
-del *.html
-echo "Done!"
-
