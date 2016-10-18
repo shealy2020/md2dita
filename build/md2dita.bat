8d06c42849bb2367fe6fad27fc1dcf3470d73182
Sean Healy <sean.healy@ingeniux.com>
2016-10-18 10:29:44 -0700

    commit .bat, .md, .ditamap

--- build/md2dita.bat
+++ build/md2dita.bat
@@ -1,15 +1,16 @@
+@echo off  
 REM cd to markdown source
-cd D:\working\git\md2dita\tbird\md
+cd %MD_HOME%\tbird\md
 REM Converts MD to HTML
 for %%x in (*.md) do multimarkdown -f -b %%x
 REM cd to h2d plugin
-cd D:\working\git\md2dita\build\h2d
+cd %MD_HOME%\build\h2d
 REM Converts HTML to DITA
-call ant -Dargs.input=D:\working\git\md2dita\tbird\md -Dargs.output=D:\working\git\md2dita\tbird\md
-cd D:\working\git\md2dita\tbird\md
+call ant -Dargs.input=%MD_HOME%\tbird\md -Dargs.output=%MD_HOME%\tbird\md
+cd %MD_HOME%\tbird\md
 REM Delete intrim HTML files
 del *.html
 REM Return to build dir
-cd D:\working\git\md2dita\build
+cd %MD_HOME%\build
 echo "Done!"
 
